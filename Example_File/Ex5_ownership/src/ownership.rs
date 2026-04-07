38e86594a2a021e3dd0d6c5aab71536eee889d9a
xmhkx <matthew.klein@my.utsa.edu>
2026-04-06 23:29:53 -0500

    added more to the example, but not finished need part 2 & 3 to complete

--- Example_File/Ex5_ownership/src/ownership.rs
+++ Example_File/Ex5_ownership/src/ownership.rs
@@ -22,12 +22,14 @@ fn main() {
         let s2 = s1; // s2 takes ownership of the string, s1, no more s1
         println!("s2: {s2}"); // this works cause s2 is in inner scope
     }
-    println!("s1: {s1}"); // this works because s1 is still in scope
+    //println!("s1: {s1}"); // this DOES not work, think why??
 
 
     let x = 10;
     let y = x; // y copies the value of x, both x and y are valid
     println!("x: {x}, y: {y}"); // this works because x and y are both on the stack as a string is in the heap
+
+    let s1 = String::from("Hello"); // think why i added this again, delete it and see the error
 
     take_a_string(s1); // s1 is moved into the function, s1 is no more valid here
     //println!("s1: {s1}"); // this will cause an error because s1 is no more valid here
@@ -35,12 +37,20 @@ fn main() {
     take_an_integer(x); // x is copied into the function, x is still valid here
     // println!("x: {x}"); // this works because x is still valid here
 
+    // this is a not so good way to do this, and references will show a better option 
+    let s1 = String::from("Hello"); // s1 owns the string "Hello"
+    let (s3, length) = calculate_length(s1); // s1 is moved into the function, s1 is no more valid here, s2 takes ownership of the string, length is the length of the string
+    println!("The length of '{s3}' is {length}"); // this works because s2 is still valid here
 
     // 2. References and Borrowing
 
     
 
+
+
+
 }
+
 
 
 fn take_a_string(s: String) { // string comes into scope right here <-
@@ -50,3 +60,9 @@ fn take_a_string(s: String) { // string comes into scope right here <-
 fn take_an_integer(i: i32) { // integer comes into scope right here <-
     println!("Taking ownership of the integer: {i}");
 }// integer leaves scope here and is dropped
+
+fn calculate_length(s: String) -> (String, usize){
+    let length = s.len(); // calculate the length of the string
+
+    (s, length) // return the string and its length as a tuple
+}

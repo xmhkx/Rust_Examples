e70494e538173833cdd0cc09140beaf7bf98c13f
xmhkx <matthew.klein@my.utsa.edu>
2026-04-07 23:49:35 -0500

    added tuples and arrayes to example 1, forgot to add them

--- Example_File/Ex1_variables/src/variables.rs
+++ Example_File/Ex1_variables/src/variables.rs
@@ -19,7 +19,7 @@ fn main() {
     // x = 10; this would cause an error since x is not mutable
 
     // basic types of variables:
-
+    
     // the : is used to specify the type of the variable
 
     let a: i32 = 20; // integer
@@ -40,6 +40,21 @@ fn main() {
     }
 
     println!("The value of shadow in the outer scope is: {}", shadow); // this will print 50
+
+
+    // tuples in rust, they are a collection of values of different types
+
+    let tuple: (i32, f64, bool) = (10, 3.14, true); // this is a tuple with three values of different types
+    let (x, y, z) = tuple; // this is destructuring the tuple into three variables x, y, z
+    let first_element = tuple.0; // this is accessing the first element of the tuple, which is 10
+    let second_element = tuple.1; // this is accessing the second element of the tuple, which is 3.14
+    let third_element = tuple.2; // this is accessing the third element of the tuple, which is true
+    println!("The value of x is: {}, y is: {}, z is: {}", x, y, z); // this will print the values of x, y, z
+
+    // arrays in rust, they are a collection of values of the same type
+    let array: [i32; 5] = [1, 2, 3, 4, 5]; // this is an array of 5 integers
+    println!("The first element of the array is: {}", array[0]); // this will print the first element of the array, which is 1
+
 
 
 
